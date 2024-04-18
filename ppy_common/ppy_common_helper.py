1d4a7941e6d460e220fbd0ead81ccbb7e080eb15
Touhid Mia <touhid@banglafighter.com>
2024-04-18 10:27:07 +0600

    Moved methods to file util

--- ppy_common/ppy_common_helper.py
+++ ppy_common/ppy_common_helper.py
@@ -1,10 +1,7 @@
 import random
 import sys
 import uuid
-from datetime import datetime
-
 from ppy_common import PPyCException
-from ppy_file_text import FileUtil
 
 
 class PyCommon:
@@ -55,22 +52,3 @@ class PyCommon:
                 raise PPyCException(error)
 
         return None
-
-    """
-        Task: Use the method for check any service started withing the time difference in second.
-        
-        Usages: It used when production mode has multiple worker, run service multiple time, so it can help to prevent 
-        multi start of a service more than once
-    """
-    @staticmethod
-    def is_started(check_file_path_name: str, time_diff_in_sec) -> bool:
-        current_time = datetime.now()
-        if FileUtil.is_exist(check_file_path_name):
-            created, modified = FileUtil.get_created_modified_datetime(check_file_path_name)
-            diff = current_time - created
-            if diff.seconds > time_diff_in_sec:
-                FileUtil.delete(path=check_file_path_name)
-            else:
-                return True
-        FileUtil.create_empty_file(path=check_file_path_name)
-        return False
