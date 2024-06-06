adfb52a3657e0868f999f6dd2ea3801602eea912
H.M.Touhid Mia (HMTMCSE) <hmtm.cse@gmail.com>
2024-06-07 04:45:49 +0600

    Added Base 64 Encoder and Decoder

--- ppy_common/ppy_common_helper.py
+++ ppy_common/ppy_common_helper.py
@@ -1,3 +1,4 @@
+import base64
 import random
 import string
 import sys
@@ -28,6 +29,24 @@ class PyCommon:
     def get_random_string(length: int = 5) -> str:
         random_string = ''.join(random.choices(string.ascii_lowercase + string.digits, k=length))
         return random_string
+
+    @staticmethod
+    def base64_encode(text, altchars=None):
+        if not text:
+            return None
+        bytes_string = text.encode('utf-8')
+        base64_bytes = base64.b64encode(bytes_string, altchars=altchars)
+        base64_string = base64_bytes.decode('utf-8')
+        return base64_string
+
+    @staticmethod
+    def base64_decode(base64_text, altchars=None):
+        if not base64_text:
+            return None
+        bytes_string = base64_text.encode('utf-8')
+        base64_bytes = base64.b64decode(bytes_string, altchars=altchars)
+        base64_string = base64_bytes.decode('utf-8')
+        return base64_string
 
     @staticmethod
     def import_from_string(import_name: str, silent: bool = False):
